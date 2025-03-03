593353bc23cb10cb54895c2dff2fafc7aaa00956
shota <Tanikawa.Shota@technopro.com>
2025-03-03 15:27:40 +0900

    first commit

---
+++ slwordbook/src/main/java/com/example/slwordbook/SlwordbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.slwordbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlwordbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlwordbookApplication.class, args);
+	}
+
+}
---
+++ slwordbook/src/test/java/com/example/slwordbook/SlwordbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slwordbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlwordbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
