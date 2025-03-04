5c330a84cd605d21f399185b73ea2d97decf73c5
sato.hayaki <satouhayaki@gmail.com>
2025-03-04 15:52:38 +0900

    Repository修正

---
+++ src/main/java/com/example/slwordbook/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.slwordbook.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.slwordbook.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
