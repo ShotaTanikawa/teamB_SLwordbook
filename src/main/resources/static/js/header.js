194f492e956114ad9d7e0873b2b04bc32d14ba20
shota <Tanikawa.Shota@technopro.com>
2025-03-04 13:58:57 +0900

    JSを消してHTMLとCSSだけにしました

--- src/main/resources/static/js/header.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const userMenu = document.querySelector(".user-name");
-    const dropdown = document.querySelector(".dropdown-menu");
-
-    userMenu.addEventListener("click", function(event) {
-        dropdown.computedStyleMap.display
-    });
-});
