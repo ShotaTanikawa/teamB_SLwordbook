f46930e68e720d7080925447f5f1d1d279b2d1bb
shota <Tanikawa.Shota@technopro.com>
2025-03-05 16:38:13 +0900

    一覧機能の作成

--- src/main/java/com/example/slwordbook/service/WordbookService.java
+++ src/main/java/com/example/slwordbook/service/WordbookService.java
@@ -1,8 +1,11 @@
 package com.example.slwordbook.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.slwordbook.model.Wordbook;
 import com.example.slwordbook.repository.WordbookRepository;
 
 @Service
@@ -11,4 +14,8 @@ public class WordbookService {
     @Autowired
     private WordbookRepository wordbookRepository;
     
+    //Get all Mywordbooks
+    public List<Wordbook> listAll() {
+        return wordbookRepository.findAll();
+    }
 }
